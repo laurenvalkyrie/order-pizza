091d1844cb4061d113e2c7deb0875ad93539f069
Lauren Posey <mrslaurenposey@gmail.com>
2016-06-17 11:34:42 -0700

    working on yourOrder protoype to calculate size cost

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,20 @@
 // ============ Backend Logic ============
 
 
-function Pizza (size, toppings) {
+function Pizza (size, toppings, cost) {
   this.pizzaSize = size;
-  this.toppings = [];
+  this.toppings = toppings;
   this.totalCost = cost;
 }
 
 Pizza.prototype.yourOrder = function () {
-
+  if (this.pizzaSize === "small") {
+    return this.price += 12;
+  } else if (this.pizzaSize === "medium") {
+    return this.price += 18;
+  } else {
+    return this.price += 22;
+  }
 }
 
 
@@ -18,7 +24,10 @@ $(function() {
   $('order-pizza'.submit(function(e) {
     e.preventDefault();
     var size = $('#pizza-size').val();
-    var toppings = $('#toppings').val();
+    // var toppings = [];
+    // $('input[name="topping":checked').each(function(){
+    //   (toppings).push(this.value);
+    // });
     var pizza = new Pizza(size, toppings, cost);
 
     $('#output'.append('<p>' + pizza.pizzaSize + '</p>', '<li>' + pizza.toppings + '</li>', '<p>' + pizza.totalCost + '</p>');
